34e70c5ecfff4a5821fac3ab4715352a26e9c556
dnminh <dnminh@photron.com.vn>
2023-07-11 14:10:13 +0700

    Change header

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 
 # Main page heading
 st.title("Object Detection using YOLOv8")
-st.header("Material Segmentation")
+st.header("Object Segmentation")
 st.subheader('')
 
 # Sidebar
